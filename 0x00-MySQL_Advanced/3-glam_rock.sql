7840770a120cddc98ade6bc8d054276731361b0a
FelixOtienoArogo <felixarogo@gmail.com>
2023-05-04 22:48:52 +0300

    2022

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,7 +2,7 @@
 -- ranked by their longevity
 
 SELECT
-band_name AS band_name, IFNULL(split, 2023) - IFNULL(formed, 0) AS lifespan
+band_name AS band_name, IFNULL(split, 2022) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
