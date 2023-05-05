e4c9edc95daaa6426dfa694cea22465302594028
FelixOtienoArogo <felixarogo@gmail.com>
2023-05-05 11:01:52 +0300

    Buy buy buy

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,10 @@
+-- creates a trigger that decreases the quantity of
+-- an item after adding a new order.
+
+
+CREATE TRIGGER new_item
+AFTER INSERT ON orders
+FOR EACH ROW
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
