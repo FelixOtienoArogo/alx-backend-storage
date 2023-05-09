079cda69fb8cc75e6994c2e8954ad6562d0e2d41
FelixOtienoArogo <felixarogo@gmail.com>
2023-05-09 21:15:30 +0300

    school

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,4 +1,12 @@
-"""schools_by_topic"""
+#!/usr/bin/env python3
+"""MongoDB Find"""
+
+
 def schools_by_topic(mongo_collection, topic):
-    """returns the list of school having a specific topic"""
+    """
+    Use an aggregate to find documents
+    :param mongo_collection: Pymongo connection
+    :param topic: The topic to search
+    :return: The list of school that having the same topics
+    """
     return [school for school in mongo_collection.find({"topics": topic})]
