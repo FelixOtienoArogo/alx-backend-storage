ce4e319590b7d11639763ff979352412af90ec15
FelixOtienoArogo <felixarogo@gmail.com>
2023-05-09 19:33:58 +0300

    8-all

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,15 +1,11 @@
-"""8-all."""
+#!/usr/bin/env python3
+""" pymongo list """
+
+import pymongo
+
+
 def list_all(mongo_collection):
-    """
-    Description.
-
-    --------------
-    function that lists all documents in a collection
-    """
-    doc_list  = []
-    
-    document = mongo_collection.find()
-
-    for doc in document:
-        doc_list.append(doc)
-    return doc_list
+    """ List all elements in a collection """
+    if not mongo_collection:
+        return []
+    return list(mongo_collection.find())
