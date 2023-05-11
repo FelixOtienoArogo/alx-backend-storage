f724f76c54a88decf1cb131473f57e30280520e9
FelixOtienoArogo <felixarogo@gmail.com>
2023-05-11 21:48:56 +0300

    last

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,35 +1,34 @@
 #!/usr/bin/env python3
-""" Module for Implementing an expiring web cache and tracker """
+"""Module for Implementing an expiring web cache and tracker."""
 
 from functools import wraps
 import redis
 import requests
 from typing import Callable
 
-client = redis.Redis()
+_redis = redis.Redis()
 
 
 def count_requests(method: Callable) -> Callable:
-    """ Decortator to count how many request has been made"""
-
+    """Decortator to count how many request has been made."""
     @wraps(method)
     def wrapper(url):
-        """ Function wrapper """
-        client.incr(f"count:{url}")
-        cached_html = client.get(f"cached:{url}")
-        if cached_html:
-            return cached_html.decode('utf-8')
-
-        html = method(url)
-        client.setex(f"cached:{url}", 10, html)
-        return html
+        """Wrap the method."""
+        key = f"count:{url}"
+        _redis.incr(key)
+        count = _redis.get(key)
+        if count is not None:
+            return count.decode('utf-8')
+        else:
+            page = method(url)
+            _redis.setex(key, 10, page)
+            return page
 
     return wrapper
 
 
 @count_requests
 def get_page(url: str) -> str:
-    """Gets the html content of a web page
-    """
+    """Get the html content of a web page."""
     req = requests.get(url)
     return req.text
